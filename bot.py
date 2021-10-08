2b8c802e1a0310215e77f0d13b95573afbd8ae7c
houstonlucas <houstonlucas@nevada.unr.edu>
2021-10-07 21:30:33 -0700

    Modified structure command to include named instance mapping.

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
-import os, sys
-
+import os
+import sys
+import json
 import discord
 import asyncio
 import boto3
+import click
 
 client = discord.Client()
 
-ec2 = boto3.resource('ec2', region_name='us-west-1')
+instance_map = {}
 
-# TODO: create a click interface
-instance = ec2.Instance(sys.argv[1])
+@click.command()
+@click.argument('instance_map_file', type=click.Path(exists=True))
+def main(instance_map_file):
+    ec2 = boto3.resource('ec2', region_name='us-west-1')
+
+    global instance_map
+    try:
+        with open(instance_map_file, "r") as f:
+            instance_map_json = json.load(f)
+    except FileNotFoundError as e:
+        print(e)
+        exit()
+
+    if len(instance_map_json) == 0:
+        raise Exception("Empty Instance Map")
+
+    for instance_name, instance_id in instance_map_json.items():
+        instance_map[instance_name] = ec2.Instance(instance_id)
+
+    client.run(os.environ['AWSDISCORDTOKEN'])
+
 
 @client.event
 async def on_ready():
@@ -18,6 +39,7 @@ async def on_ready():
     print(client.user.id)
     print('------------')
 
+
 @client.event
 async def on_message(message):
     print(message)
@@ -25,27 +47,38 @@ async def on_message(message):
     if client.user.id in memberIDs and message.channel.name == "minecraft-upper":
         print(message.content)
         channel = message.channel
-        if 'stop' in message.content:
-            if turnOffInstance():
-                await channel.send("AWS Instance stopping")
-            else:
-                await channel.send('Error stopping AWS Instance')
-        elif 'start' in message.content:
-            if turnOnInstance():
-                await channel.send('AWS Instance starting')
-            else:
-                await channel.send('Error starting AWS Instance')
-        elif 'status' in message.content:
-            await channel.send('AWS Instance status is: ' + getInstanceState())
+        target_instance_name = message.content.split()[1]
 
-def turnOffInstance():
+        if target_instance_name in instance_map:
+            target_instance = instance_map[target_instance_name]
+
+            if 'stop' in message.content:
+                if turnOffInstance(target_instance):
+                    await channel.send("AWS Instance stopping")
+                else:
+                    await channel.send('Error stopping AWS Instance')
+            elif 'start' in message.content:
+                if turnOnInstance(target_instance):
+                    await channel.send('AWS Instance starting')
+                else:
+                    await channel.send('Error starting AWS Instance')
+            elif 'status' in message.content:
+                await channel.send('AWS Instance status is: ' + getInstanceState(target_instance))
+
+        else:
+            await channel.send(f"Could not find {target_instance}")
+
+
+def turnOffInstance(instance):
     try:
         instance.stop(False, False)
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
-def turnOnInstance():
+
+def turnOnInstance(instance):
     try:
         instance.start()
         return True
@@ -53,15 +86,19 @@ def turnOnInstance():
         print(e)
         return False
 
-def getInstanceState():
+
+def getInstanceState(instance):
     return instance.state['Name']
 
-def rebootInstance():
+
+def rebootInstance(instance):
     try:
         instance.reboot()
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
-client.run(os.environ['AWSDISCORDTOKEN'])
+if __name__ == '__main__':
+    main()
